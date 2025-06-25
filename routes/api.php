9e8b74ace229716d5e99631db30514ca8ff47a49
Ken Waribo <degee147@gmail.com>
2025-06-25 11:14:56 +0100

    Installation and Auth setup

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+// 👇 Import auth routes into API group
+require __DIR__ . '/auth.php';
